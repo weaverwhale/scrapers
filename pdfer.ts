b4f42ff21ea5a502541539b036fe4e1881285179
Mike Weaver <michael@triplewhale.com>
2024-03-04 11:27:09 -0500

    PDF with only header & footer

--- pdfer.ts
+++ pdfer.ts
@@ -21,7 +21,7 @@ const REPORT_ADMIN_PWD = 'TestPassword1'
 const willyDashId = 'AmmcFZ69sXEyXk13HYSe'
 const shopDomain = 'madisonbraids.myshopify.com'
 
-const viewportSize = { width: 2480, height: 3508 }
+const viewportSize = { width: 1240, height: 1754 }
 
 export const createDashboardPDF = async () => {
   const browser = await puppeteer.launch({
@@ -37,6 +37,7 @@ export const createDashboardPDF = async () => {
   logger.info('pdf: started')
   const appLink = 'https://app.triplewhale.com'
   const url = `${appLink}/dashboards/${willyDashId}?shop-id=${shopDomain}`
+
   await page.goto(`${appLink}/signin`, { waitUntil: 'domcontentloaded' })
   await page.waitForSelector('#login-email-input', {
     timeout: 10000,
@@ -45,32 +46,29 @@ export const createDashboardPDF = async () => {
   await page.type('#login-password-input', REPORT_ADMIN_PWD)
   await sleep(2000)
 
-  logger.info('pdf: trying to log in')
   await page.click('.continue-button')
   logger.info('pdf: logged in')
   await sleep(2000)
 
-  logger.info('pdf: loading page')
+  logger.info('pdf: page loading')
   await page.goto(url, {
     waitUntil: 'networkidle2',
   })
-  logger.info('pdf: page loaded: ' + url)
 
-  // const screenshotFileName = `${willyDashId}_${new Date()}.jpg`;
-  const elementToScreenshot = `.w-full.h-full[class*="container"]` //slick!
-  await page.$(elementToScreenshot)
-  // await mainElement?.screenshot({
-  //   path: screenshotFileName,
-  //   fullPage: true,
-  // });
-  logger.info('pdf: element found & sreenshotted: ' + elementToScreenshot)
+  logger.info('pdf: page loaded; creating pdf')
+  await page.evaluate(() => {
+    const header = document.querySelector('.willy-dash-header')
+    const mainPane = document.querySelector('.willy-main-pane')
+    document.body.innerHTML = `<div>${header?.outerHTML}${mainPane?.outerHTML}</div>`
+  })
 
   const pdfFileName = `${willyDashId}_${new Date()}.pdf`
-  const pdfFile = await page.pdf({ ...viewportSize, path: pdfFileName })
+  const pdfFile = await page.pdf({ format: 'a4', path: pdfFileName })
   fs.writeFileSync('pdfs/' + pdfFileName, pdfFile)
+  fs.unlinkSync(pdfFileName)
+  logger.info('pdf: created & saved')
 
   await browser.close()
-
   logger.info('pdf: finished ' + willyDashId)
 }
 
